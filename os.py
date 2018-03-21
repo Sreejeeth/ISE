f80efbeade39d159994c73afda0d8d71f4b8316a
Sreejeeth <sreejeeth.ramprasad1996@gmail.com>
2018-03-21 11:55:09 +0530

    worked on cleaning the code

--- os.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT=os.path.join(BASE_DIR,"static")
-STATICFILES_DIRS =os.path.join(os.path.dirname(__file__),"static")
-BASE1_DIR=os.path.join(BASE_DIR,"mvit")
-print(BASE1_DIR)
-print(STATIC_ROOT)
